6662e522db1cb214dedd04823fb1c6f37886ae30
jantar44 <niemno90@op.pl>
2020-05-02 14:44:57 +0200

    cleanup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+   name='blood-donation',
+   version='0.1',
+   description='The goal is to predict the last column, whether he/she donated blood in March 2007',
+   author='Me',
+   packages=['blood-donation'],
+   install_requires=['matplotlib', 'numpy','pandas','sklearn'],
